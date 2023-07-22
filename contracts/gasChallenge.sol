c34eb6144156e80c9d670aa6b57662b6ad47dccc
norr1x <85332696+x-radd@users.noreply.github.com>
2023-07-22 20:51:15 +0530

    Update gasChallenge.sol

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract gasChallenge {
-    //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    // Fixed-Size Array Technique
+    uint[10] numbers = [1,2,3,4,5,6,7,8,9,10];
     
-    //Function to check for sum of array
-    //No changes required in this function
+    // Function to check for sum of array
+    // No changes required in this function
     function getSumOfArray() public view returns (uint256) {
         uint sum = 0;
         for (uint i = 0; i < numbers.length; i++) {
@@ -21,9 +21,15 @@ contract gasChallenge {
         }
     }
     
-    //Implement Remaining Gas Optimization Techniques Here
-    //Sum of elements in the numbers array should equal 0
+    // Remaining Gas Optimization Techniques
+    // Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        // Uncheck Block
+        unchecked {
+            // Different For Loop Increment Syntax
+            for (uint i = numbers.length; i > 0; i--) {
+                numbers[i-1] = 0;
+            }
+        }
     }
- }
+}
